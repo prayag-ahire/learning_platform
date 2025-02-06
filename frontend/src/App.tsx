f542a3520466783f4e5ce06cf63a68eee281e49f
prayag <prayagahire365@gmail.com>
2025-02-07 01:21:01 +0530

    test01 for get request from backend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,23 @@
+import axios from 'axios'
 import './App.css'
+import { useState } from 'react'
 
 function App() {
 
+const [ans,setAns] = useState("");
+
+const handler = async ()=>{
+  const response = await axios.get("https://learning-platform-1oks.onrender.com/api/v1/user");
+  setAns(JSON.stringify(response.data,null,2));
+}
   return (
     <>
       <div className='text-center text-3xl mt-10 '> welcome to learn with learning-platform.</div>
+      <button className='bg-blue-500 text-white px-4 py-2 rounded mt-5' onClick={handler}>Click me</button>
+      <button onClick={()=>{
+        
+      }}></button>
+      <div>{ans}hahah</div>
     </>
   )
 }
