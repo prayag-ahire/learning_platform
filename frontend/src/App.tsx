8e57c38b5efd3facf28c44d547e45ccb6b1dbb72
prayag <prayagahire365@gmail.com>
2025-02-07 00:31:24 +0530

    init frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='text-center text-3xl mt-10 '> welcome to learn with learning-platform.</div>
+    </>
+  )
+}
+
+export default App
